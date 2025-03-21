4deaf4d54374d7697bd88d72523a3771744f5801
sabilafirdias <sabilafirdias@gmail.com>
2025-03-21 14:55:31 +0700

    menambahkan fungsi dgvMahasiswa_CellContentClick untuk menampilkan data yang dipilih ke input form

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -199,3 +199,20 @@ namespace CRUDSederhana
                 MessageBoxIcon.Information
                 );
         }
+
+        private void dgvMahasiswa_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dgvMahasiswa.Rows[e.RowIndex];
+
+                //Coba gunakan indeks jika "NIM" tidak ditemukan
+                txtNIM.Text = row.Cells[0].Value.ToString();
+                txtNama.Text = row.Cells[1].Value.ToString();
+                txtEmail.Text = row.Cells[2].Value.ToString();
+                txtTelepon.Text = row.Cells[3].Value.ToString();
+                txtAlamat.Text = row.Cells[4].Value.ToString();
+            }
+        }
+    }
+}
