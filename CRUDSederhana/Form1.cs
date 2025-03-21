51ace8b9f59cb17316a8bc5871f74dcecf5585be
sabilafirdias <sabilafirdias@gmail.com>
2025-03-21 14:36:06 +0700

    Add project files.

---
+++ CRUDSederhana/Form1.cs
@@ -0,0 +1,33 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUDSederhana
+{
+    public partial class Form1 : Form
+    {
+        static string connectionString = string.Format(
+            "Server=127.0.0.1; database = organisasiMahasiswa; UID = root; Password =saninmadi");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+        
